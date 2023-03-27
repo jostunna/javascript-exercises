02858579c025e7a967759113c15a50d0ffb864e5
jostunna <joelmubirru@gmail.com>
2023-03-27 16:23:41 +0300

    Test works with century years divisible by 400

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
 const leapYears = function(year) {
-    if ((year % 4 === 0) && (year % 100 !== 0)) { return true; } else { return false;}
+    if (((year % 400 === 0) && (year % 100 === 0)) || ((year % 4 === 0) && (year % 100 !== 0))) { return true; } else { return false;}
 };
 
 // Do not edit below this line
