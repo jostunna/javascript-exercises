e1f75831ea6770c40bc5658f901d4378b69ca824
jostunna <joelmubirru@gmail.com>
2023-03-27 14:35:32 +0300

    Test returns ERROR with negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
 const sumAll = function (min, max) {
   let sum = 0;
+  if ((typeof min !== 'number' || typeof max !== 'number') || (min < 0 || max < 0)) {
+    return 'ERROR';
+  }
   if (min > max) {
     for (let i = min; i >= max; i--) {
       sum += i;
