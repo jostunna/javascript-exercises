deedf7647e34cc983aa333d048406e490d70f3fa
jostunna <joelmubirru@gmail.com>
2023-03-27 12:51:23 +0300

    Create removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...list) {
+  for (let i = 0; i < array.length; i++) {
+    for (let item of list) {
+        if (array[i] === item) {
+            array.splice(i, 1);
+        }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
