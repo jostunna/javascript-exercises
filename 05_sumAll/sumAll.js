6f5077c2663ba5b2e2a5c489f546ac7422b17d7a
jostunna <joelmubirru@gmail.com>
2023-03-27 14:28:50 +0300

    Test works with larger number first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,16 @@
-const sumAll = function(min, max) {
-    let sum = 0;
-    for (let i = min; i <= max; i++) {
-        sum += i;
+const sumAll = function (min, max) {
+  let sum = 0;
+  if (min > max) {
+    for (let i = min; i >= max; i--) {
+      sum += i;
     }
-    return sum;
+  } else {
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
