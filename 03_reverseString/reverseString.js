127bb8186e711286f13c98a97b00e2437dd5a04b
jostunna <joelmubirru@gmail.com>
2023-03-27 12:12:17 +0300

    Refactor reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,4 @@
 const reverseString = function (word) {
-  if (word.includes(" ")) {
-    word.split(" ").map(w => w.split('').reverse().join('')).reverse().join(" ");
-  }
   return word.split("").reverse().join("");
 };
 
