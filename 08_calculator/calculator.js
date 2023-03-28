2d825ee59f1999f2f7f770ee602dbba1fb5f33de
jostunna <joelmubirru@gmail.com>
2023-03-28 15:39:20 +0300

    Implement factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -32,7 +32,12 @@ const power = function(base, exponent) {
   }	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 	
 };
 
