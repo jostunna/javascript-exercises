7646718344f2b0de471f919c1aa7aaa89a94b8e0
jostunna <joelmubirru@gmail.com>
2023-03-28 13:50:23 +0300

    Refactor to math round

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,17 +1,13 @@
-const convertToCelsius = function(fahrenheit) {
-  let celsius = (fahrenheit -32) * 5/9;
-  if (!Number.isInteger(celsius)) return +celsius.toFixed(1);
-  return celsius;
+const convertToCelsius = function (fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function(celsius) {
-  let fahrenheit = (celsius * 1.8) + 32;
-  if (!Number.isInteger(fahrenheit)) return +fahrenheit.toFixed(1);
-  return fahrenheit;
+const convertToFahrenheit = function (celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
