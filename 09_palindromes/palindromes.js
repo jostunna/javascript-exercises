73ac442c032fcd2bd12961cbf157deb482bce26c
jostunna <joelmubirru@gmail.com>
2023-03-28 18:05:42 +0300

    Implement palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let reversed = word.split("").reverse().join("");
+    return word === reversed;
 };
 
 // Do not edit below this line
