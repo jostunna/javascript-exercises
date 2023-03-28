3ac2bd12c78a950c11311a848e27e9c2b7a1662b
jostunna <joelmubirru@gmail.com>
2023-03-28 15:15:32 +0300

    Implement sum function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,12 @@ const subtract = function(num1, num2) {
   return num1 - num2;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  for (let item of array) {
+    total += item;
+  }
+  return total;	
 };
 
 const multiply = function() {
