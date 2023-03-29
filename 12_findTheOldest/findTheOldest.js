d9b689e390e83f4a673b81abcc2b2b2bfe4f3e7f
jostunna <joelmubirru@gmail.com>
2023-03-29 10:54:46 +0300

    Implement findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((prev, current) => {
+        return (prev.yearOfDeath - prev.yearOfBirth) > (current.yearOfDeath - current.yearOfBirth) ? prev : current;
+    });
 };
 
 // Do not edit below this line
